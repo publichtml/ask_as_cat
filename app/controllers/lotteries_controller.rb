518f4df771e816f683b7c42fff17565a9303f3b1
publichtml <publi9html@gmail.com>
2015-08-09 20:19:53 +0900

    Create LotteriesController with empty actions

---
+++ app/controllers/lotteries_controller.rb
@@ -0,0 +1,29 @@
+class LotteriesController < ApplicationController
+  def index
+    # TODO: implement
+  end
+
+  def new
+    # TODO: implement
+  end
+
+  def create
+    # TODO: implement
+  end
+
+  def edit
+    # TODO: implement
+  end
+
+  def update
+    # TODO: implement
+  end
+
+  def show
+    # TODO: implement
+  end
+
+  def destroy
+    # TODO: implement
+  end
+end
