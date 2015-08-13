fc6f59b6e535e39b914f9acd68e37988c0dc2fcd
publichtml <publi9html@gmail.com>
2015-08-13 13:42:46 +0900

    Validate Lottery name and winners_count

--- app/models/lottery.rb
+++ app/models/lottery.rb
@@ -1,2 +1,4 @@
 class Lottery < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :winners_count, presence: true, numericality: { only_integer: true, greater_than: 0, allow_blank: true }
 end
