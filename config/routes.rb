62e25bf6bf0032e95f0dd4db46e4c34f92ec5c13
publichtml <publi9html@gmail.com>
2015-08-15 20:40:04 +0900

    Add routes for cadidates

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root to: 'lotteries#index'
 
-  resources :lotteries
+  resources :lotteries do
+    controller :candidates do
+      get 'candidates', action: 'index'
+      put 'candidates', action: 'update'
+      get 'candidates/edit', action: 'edit'
+    end
+  end
 end
