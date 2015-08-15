d03d75b1c30695965cf9f736e5b39139dd06183a
publichtml <publi9html@gmail.com>
2015-08-16 01:58:35 +0900

    Enable to create and update candidates

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -9,7 +9,19 @@ class CandidatesController < ApplicationController
     10.times { @lottery.candidates.build }
   end
 
+  def update
+    if @lottery.update(candidates_params)
+      redirect_to lottery_candidates_path(lottery: @lottery), notice: I18n.t('messages.updated')
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def candidates_params
+    params.require(:lottery).permit(candidates_attributes: [:id, :name, :weight])
+  end
 
   def set_lottery
     @lottery = Lottery.find(params[:lottery_id])
--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,2 +1,12 @@
 class Candidate < ActiveRecord::Base
+  WEIGHTS = I18n.t('terms.choices.weight').keys.freeze
+
+  default_scope { order(:id) }
+
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :weight, presence: true, inclusion: { in: WEIGHTS }
+
+  def self.weight_choices
+    I18n.t('terms.choices.weight').invert
+  end
 end
