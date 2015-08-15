69fd5636393263887ecf8c86e9527b15e03f51c0
publichtml <publi9html@gmail.com>
2015-08-16 02:14:50 +0900

    Display index of lottery candidates

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -6,7 +6,17 @@ class Candidate < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 255 }
   validates :weight, presence: true, inclusion: { in: WEIGHTS }
 
-  def self.weight_choices
-    I18n.t('terms.choices.weight').invert
+  class << self
+    def human_weight(weight)
+      I18n.t("terms.choices.weight")[weight]
+    end
+
+    def weight_choices
+      I18n.t('terms.choices.weight').invert
+    end
+  end
+
+  def human_weight
+    self.class.human_weight(weight)
   end
 end
