cfff438b99d66a9a55b1b710e8daf7cf8d291e5b
publichtml <publi9html@gmail.com>
2015-08-16 01:12:51 +0900

    Lottery has_many candidates

--- app/models/lottery.rb
+++ app/models/lottery.rb
@@ -1,4 +1,7 @@
 class Lottery < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 255 }
   validates :winners_count, presence: true, numericality: { only_integer: true, greater_than: 0, allow_blank: true }
+
+  has_many :candidates, dependent: :destroy
+  accepts_nested_attributes_for :candidates
 end
