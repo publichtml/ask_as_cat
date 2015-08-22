cefda5d98e71824b5935fc29cf8aac562afbfcee
publichtml <publi9html@gmail.com>
2015-08-22 16:07:24 +0900

    Enable to draw lottery from lottery show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'lotteries#index'
 
   resources :lotteries do
+    patch 'draw'
     controller :candidates do
       get 'candidates', action: 'index'
       put 'candidates', action: 'update'
