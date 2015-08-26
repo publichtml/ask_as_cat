525d88061cc2b18330b2cff98268f288c500f397
publichtml <publi9html@gmail.com>
2015-08-26 21:55:25 +0900

    Move js code to js file from view

---
+++ app/assets/javascripts/reveal_after_load.js
@@ -0,0 +1,14 @@
+Reveal.initialize({
+  controls: true,
+  progress: true,
+  history: true,
+  center: true,
+  transition: 'slide' // none/fade/slide/convex/concave/zoom
+});
+
+Reveal.addEventListener('ready', function(event) {
+  Reveal.configure({ center: event.currentSlide.classList.contains("center") })
+});
+Reveal.addEventListener('slidechanged', function(event) {
+  Reveal.configure({ center: event.currentSlide.classList.contains("center") })
+});
